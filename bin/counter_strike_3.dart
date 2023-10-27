7aed53b3077f10f58a68e93468bdb6f383c3b810
boginni <brunnomarqueszr1@gmail.com>
2023-10-27 03:47:21 -0300

    feat: setup project

--- bin/counter_strike_3.dart
+++ bin/counter_strike_3.dart
@@ -1,5 +1,5 @@
-import 'package:counter_strike_3/counter_strike_3.dart' as counter_strike_3;
-
-void main(List<String> arguments) {
-  print('Hello world: ${counter_strike_3.calculate()}!');
-}
+// import 'package:counter_strike_3/counter_strike_3.dart' as counter_strike_3;
+//
+// void main(List<String> arguments) {
+//   print('Hello world: ${counter_strike_3.calculate()}!');
+// }
--- test/counter_strike_3_test.dart
+++ test/counter_strike_3_test.dart
@@ -1,8 +1,8 @@
-import 'package:counter_strike_3/counter_strike_3.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
+// import 'package:counter_strike_3/counter_strike_3.dart';
+// import 'package:test/test.dart';
+//
+// void main() {
+//   test('calculate', () {
+//     expect(calculate(), 42);
+//   });
+// }
